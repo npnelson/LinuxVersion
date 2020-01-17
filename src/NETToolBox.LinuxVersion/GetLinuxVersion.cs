90eae87cdefc9f039064ebfac94bb7526d2afdff
npnelson <nnelson1@insight.rr.com>
2020-01-17 08:21:08 -0500

    cache result of call

--- src/NETToolBox.LinuxVersion/GetLinuxVersion.cs
+++ src/NETToolBox.LinuxVersion/GetLinuxVersion.cs
@@ -6,18 +6,24 @@ namespace NETToolBox.LinuxVersion
 {
     public static class GetLinuxVersion
     {
+        static LinuxVersionInfo? _versioninfo;
         public static LinuxVersionInfo GetLinuxVersionInfo()
         {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) throw new InvalidOperationException("Do not call GetLinuxVersionInfo when not running on Linux");
-            var kernel = RuntimeInformation.OSDescription;
-            var osReleaseOutput = GetCommandOutput("/bin/cat", "/etc/os-release");
-            string? osVersion=null;
-            if (osReleaseOutput.Contains("debian", StringComparison.OrdinalIgnoreCase))
+            if (_versioninfo != null) return _versioninfo; //no need to run the commands on every call, so we will only run the first time and cache the result
+            else
             {
-                osVersion = GetCommandOutput("/bin/cat", "/etc/debian_version");
+                if (!RuntimeInformation.IsOSPlatform(OSPlatform.Linux)) throw new InvalidOperationException("Do not call GetLinuxVersionInfo when not running on Linux");
+                var kernel = RuntimeInformation.OSDescription;
+                var osReleaseOutput = GetCommandOutput("/bin/cat", "/etc/os-release");
+                string? osVersion = null;
+                if (osReleaseOutput.Contains("debian", StringComparison.OrdinalIgnoreCase))
+                {
+                    osVersion = GetCommandOutput("/bin/cat", "/etc/debian_version");
+                }
+                var retval = new LinuxVersionInfo(kernel, osReleaseOutput, osVersion);
+                _versioninfo = retval;
+                return retval;
             }
-            var retval = new LinuxVersionInfo(kernel, osReleaseOutput, osVersion);
-            return retval;
         }
 
         private static string GetCommandOutput(string command, string arguments)
